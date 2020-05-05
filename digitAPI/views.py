086e14c4ba0e68d0414b411207e0a2691da68783
josenarvaezp <jose.narvaez.p@gmail.com>
2020-05-05 14:50:10 +0100

    make better

--- digitAPI/views.py
+++ digitAPI/views.py
@@ -136,183 +136,6 @@ import tensorflow as tf
 
 
 
-@csrf_exempt
-def detect(request):
-    # initialize the data dictionary to be returned by the request
-    data = {"success": False}
-
-    # check to see if this is a post request
-    if request.method == "POST":
-        
-        try:
-            #GETTING MODEL
-            model_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'saved_model/digit_detection_mnist6')
-            model = tf.keras.models.load_model(model_path)
-
-            #GETTING IMAGE FROM POST DATA
-            body = json.loads(request.body.decode("utf-8"))
-
-            base64_string = body['image']
-            decoded_data = base64.b64decode(base64_string)
-            np_data = np.frombuffer(decoded_data,np.uint8)
-            # Grayscale image
-            image = cv2.imdecode(np_data,cv2.IMREAD_GRAYSCALE)
-            data.update({"success": True ,"size": image.shape})
-
-            prediction_count = 1
-            #crop image according to bounding boxes
-            bounding_boxes = body['boxes']
-            for box in bounding_boxes:
-                x1 = int(box[0])
-                y1 = int(box[1])
-                x2 = int(box[2])
-                y2 = int(box[3])
-                # data.update({"x1": x1, "y1": y1, "x2": x2, "y2": y2})
-
-                temp_img = image[y1:y2, x1:x2]
-                # data.update({"success": True ,"size2": temp_img.shape})
-
-                prediction = predict_digits(temp_img, model)
-                prediction_string = "prediction"+str(prediction_count)
-                data.update({"'"+prediction_string+"'": prediction})
-                prediction_count = prediction_count+1
-
-            # #PREDICTION
-            # # prediction = predict_digits(image, model)
-
-            # #UPDATE DATA
-            # data.update({"success": True ,"prediction": prediction})
-        except:
-            data.update({"success": True ,"prediction": "ERROR"})
-
-    return JsonResponse(data)
-
-def predict_digit(img, model):
-    prediction_input = np.array([img])
-    prediction = model.predict(prediction_input)
-    prediction = np.argmax(prediction)
-    return prediction
-
-def pre_process(img):
-    # img = img[1760:1960,1315:1580]
-    # img = img[2935:3180, 2500:2865]
-    # img = img[ 2980:3210, 365:870]
-    thresh = cv2.threshold(img, 0, 255,cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU)[1]
-    kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (1, 5))
-    thresh = cv2.morphologyEx(thresh, cv2.MORPH_OPEN, kernel)
-    thresh = thresh/255.0
-    thresh = thresh.astype(int)
-    #invert colours
-    if (np.bincount(thresh.flatten()).argmax()) == 1:
-        thresh = 1-thresh
-    
-    return thresh
-
-def extract_digits(img):
-    # storing upper left corner and down right corner
-    coordinates = []
-
-    # setup
-    interval = 1
-    initial = 0
-    l = 0
-    r = 0
-
-    row, column = img.shape
-    index = 0
-
-    coordinate = []
-
-    while (initial+interval < column):
-        current_frame = img[:,initial:initial+interval]
-        next_frame = img[:, initial+interval:initial+interval+interval]
-
-        if (np.count_nonzero(current_frame == 1) == 0 and np.count_nonzero(next_frame == 1) != 0 and l == r ):
-            temp_list = []
-            l_x = initial+interval
-            coordinate.append(l_x)
-            l=l+1
-            
-
-        if ((np.count_nonzero(current_frame == 1) != 0 and np.count_nonzero(next_frame == 1) == 0 and r == l-1 ) or (initial+interval == column-interval and r == l-1 )):
-            r_x = initial+interval
-            coordinate.append(r_x)
-            coordinates.append(coordinate)
-            coordinate = []
-            r=r+1
-
-        # increase frame
-        initial = initial+interval
-
-    return coordinates
-
-def predict_digits(img, model):
-
-    img = pre_process(img)
-    coordinates = extract_digits(img)
-    
-    prediction_list = []
-    
-    for coordinate in coordinates:
-        l_x = coordinate[0]
-        r_x = coordinate[1]
-
-        if (r_x - l_x < 10):
-            continue
-
-        temp_img = img[:,l_x:r_x]
-        y,x = temp_img.shape
-
-        # add half and half
-        to_add = int((y - x)/2)
-        diff = y - (x+ (to_add *2))
-        to_add_left = to_add
-        to_add_right = to_add + diff
-        
-        temp_img = np.append(temp_img, np.zeros((y,to_add_left )), axis=1)
-        temp_img = np.append(np.zeros((y,to_add_right )),temp_img, axis=1)
-
-        temp_img = cv2.resize(temp_img,(64,64))
-        thresh_1 = temp_img > 0.3
-        thresh_2 = temp_img <= 0.3
-        temp_img[thresh_1] = 1
-        temp_img[thresh_2] = 0
-        
-        temp_img = np.dstack(temp_img).T
-
-        prediction = predict_digit(temp_img, model)
-        prediction_list.append(str(prediction))
-
-    return prediction_list
-
-
-
-def _grab_image(path=None, stream=None, url=None):
-    # if the path is not None, then load the image from disk
-    if path is not None:
-        image = imread(path, cv2.IMREAD_GRAYSCALE)
-        # image = process_img(image)
-        
-    # otherwise, the image does not reside on disk
-    else:	
-        # if the URL is not None, then download the image
-        if url is not None:
-            resp = urllib.urlopen(url)
-            data = resp.read()
-        # if the stream is not None, then the image has been uploaded
-        elif stream is not None:
-            data = stream.read()
-        # convert the image to a NumPy array and then read it into
-        # OpenCV format
-        image = np.asarray(bytearray(data), dtype="uint8")
-        image = cv2.imdecode(image, cv2.IMREAD_GRAYSCALE)
-        # image = process_img(image)
-    # return the image
-    return image
-
-
-
-
 # @csrf_exempt
 # def detect(request):
 #     # initialize the data dictionary to be returned by the request
@@ -323,28 +146,11 @@ def _grab_image(path=None, stream=None, url=None):
         
 #         try:
 #             #GETTING MODEL
-#             model_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'saved_model/digit_detection')
+#             model_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'saved_model/digit_detection_mnist6')
 #             model = tf.keras.models.load_model(model_path)
 
 #             #GETTING IMAGE FROM POST DATA
 #             body = json.loads(request.body.decode("utf-8"))
-
-
-#             # # check to see if an image was uploaded
-#             # if request.FILES.get("image", None) is not None:
-#             #     # grab the uploaded image
-#             #     image = _grab_image(stream=request.FILES["image"])
-#             #     # otherwise, assume that a URL was passed in
-#             # else:
-#             #     # grab the URL from the request
-#             #     url = request.POST.get("url", None)
-#             #     # if the URL is None, then return an error
-#             #     if url is None:
-#             #         data["error"] = "No URL provided."
-#             #         return JsonResponse(data)
-#             #     # load the image and convert
-#             #     image = _grab_image(url=url)
-
 
 #             base64_string = body['image']
 #             decoded_data = base64.b64decode(base64_string)
@@ -352,17 +158,6 @@ def _grab_image(path=None, stream=None, url=None):
 #             # Grayscale image
 #             image = cv2.imdecode(np_data,cv2.IMREAD_GRAYSCALE)
 #             data.update({"success": True ,"size": image.shape})
-
-#             # prediction_count = 1
-#             # # #crop image according to bounding boxes
-#             # # box = body['boxes']
-#             # x1 = box[0][0]
-#             # y1 = box[0][1]
-#             # x2 = box[0][2]
-#             # y2 = box[0][3]
-#             # image = image[y1:y2, x1:x2]
-#             # prediction = predict_digits(image, model)
-#             # data.update({"success": True ,"prediction": prediction})
 
 #             prediction_count = 1
 #             #crop image according to bounding boxes
@@ -372,14 +167,15 @@ def _grab_image(path=None, stream=None, url=None):
 #                 y1 = int(box[1])
 #                 x2 = int(box[2])
 #                 y2 = int(box[3])
-#                 data.update({"x1": x1, "y1": y1, "x2": x2, "y2": y2})
+#                 # data.update({"x1": x1, "y1": y1, "x2": x2, "y2": y2})
 
 #                 temp_img = image[y1:y2, x1:x2]
-#                 data.update({"success": True ,"size2": temp_img.shape})
+#                 # data.update({"success": True ,"size2": temp_img.shape})
 
 #                 prediction = predict_digits(temp_img, model)
-#                 data.update({"prediction": prediction})
-#                 # prediction_count = prediction_count+1
+#                 prediction_string = "prediction"+str(prediction_count)
+#                 data.update({"'"+prediction_string+"'": prediction})
+#                 prediction_count = prediction_count+1
 
 #             # #PREDICTION
 #             # # prediction = predict_digits(image, model)
@@ -477,6 +273,12 @@ def _grab_image(path=None, stream=None, url=None):
 #         temp_img = np.append(np.zeros((y,to_add_right )),temp_img, axis=1)
 
 #         temp_img = cv2.resize(temp_img,(64,64))
+#         thresh_1 = temp_img > 0.3
+#         thresh_2 = temp_img <= 0.3
+#         temp_img[thresh_1] = 1
+#         temp_img[thresh_2] = 0
+        
+#         temp_img = np.dstack(temp_img).T
 
 #         prediction = predict_digit(temp_img, model)
 #         prediction_list.append(str(prediction))
@@ -507,6 +309,185 @@ def _grab_image(path=None, stream=None, url=None):
 #         # image = process_img(image)
 #     # return the image
 #     return image
+
+
+
+@csrf_exempt
+def detect(request):
+    # initialize the data dictionary to be returned by the request
+    data = {"success": False}
+
+    # check to see if this is a post request
+    if request.method == "POST":
+        
+        try:
+            #GETTING MODEL
+            model_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'saved_model/digit_detection_mnist6')
+            model = tf.keras.models.load_model(model_path)
+
+            #GETTING IMAGE FROM POST DATA
+            body = json.loads(request.body.decode("utf-8"))
+
+            base64_string = body['image']
+            decoded_data = base64.b64decode(base64_string)
+            np_data = np.frombuffer(decoded_data,np.uint8)
+            # Grayscale image
+            image = cv2.imdecode(np_data,cv2.IMREAD_GRAYSCALE)
+            data.update({"success": True ,"size": image.shape})
+
+            prediction_count = 1
+            #crop image according to bounding boxes
+            bounding_boxes = body['boxes']
+            for box in bounding_boxes:
+                x1 = int(box[0])
+                y1 = int(box[1])
+                x2 = int(box[2])
+                y2 = int(box[3])
+                # data.update({"x1": x1, "y1": y1, "x2": x2, "y2": y2})
+
+                temp_img = image[y1:y2, x1:x2]
+                # data.update({"success": True ,"size2": temp_img.shape})
+
+                prediction = predict_digits(temp_img, model)
+                prediction_string = "prediction"+str(prediction_count)
+                data.update({"'"+prediction_string+"'": prediction})
+                prediction_count = prediction_count+1
+
+            # #PREDICTION
+            # # prediction = predict_digits(image, model)
+
+            # #UPDATE DATA
+            # data.update({"success": True ,"prediction": prediction})
+        except:
+            data.update({"success": True ,"prediction": "ERROR"})
+
+    return JsonResponse(data)
+
+def predict_digit(img, model):
+    prediction_input = np.array([img])
+    prediction = model.predict(prediction_input)
+    prediction = np.argmax(prediction)
+    return prediction
+
+def pre_process(img):
+    thresh = cv2.threshold(img, 0, 255,cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU)[1]
+    kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (1, 5))
+    thresh = cv2.morphologyEx(thresh, cv2.MORPH_OPEN, kernel)
+    thresh = thresh/255.0
+    thresh = thresh.astype(int)
+    #invert colours
+    if (np.bincount(thresh.flatten()).argmax()) == 1:
+        thresh = 1-thresh
+    
+    return thresh
+
+def extract_digits(img):
+    # storing upper left corner and down right corner
+    coordinates = []
+
+    # setup
+    interval = 1
+    initial = 0
+    l = 0
+    r = 0
+
+    row, column = img.shape
+    index = 0
+
+    coordinate = []
+
+    while (initial+interval < column):
+        current_frame = img[:,initial:initial+interval]
+        next_frame = img[:, initial+interval:initial+interval+interval]
+
+        if (np.count_nonzero(current_frame == 1) == 0 and np.count_nonzero(next_frame == 1) != 0 and l == r ):
+            temp_list = []
+            l_x = initial+interval
+            coordinate.append(l_x)
+            l=l+1
+            
+
+        if ((np.count_nonzero(current_frame == 1) != 0 and np.count_nonzero(next_frame == 1) == 0 and r == l-1 ) or (initial+interval == column-interval and r == l-1 )):
+            r_x = initial+interval
+            coordinate.append(r_x)
+            coordinates.append(coordinate)
+            coordinate = []
+            r=r+1
+
+        # increase frame
+        initial = initial+interval
+
+    return coordinates
+
+def predict_digits(img, model):
+
+    img = pre_process(img)
+    # coordinates = extract_digits(img)
+    
+    temp_img = cv2.resize(img,(64,64))
+
+    temp_img = np.dstack(temp_img).T
+
+    prediction = predict_digit(temp_img, model)
+    prediction_list = []
+    prediction_list.append(str(prediction))
+    
+    # for coordinate in coordinates:
+    #     l_x = coordinate[0]
+    #     r_x = coordinate[1]
+
+    #     if (r_x - l_x < 10):
+    #         continue
+
+    #     temp_img = img[:,l_x:r_x]
+    #     y,x = temp_img.shape
+
+    #     # add half and half
+    #     to_add = int((y - x)/2)
+    #     diff = y - (x+ (to_add *2))
+    #     to_add_left = to_add
+    #     to_add_right = to_add + diff
+        
+    #     temp_img = np.append(temp_img, np.zeros((y,to_add_left )), axis=1)
+    #     temp_img = np.append(np.zeros((y,to_add_right )),temp_img, axis=1)
+
+    #     temp_img = cv2.resize(temp_img,(64,64))
+    #     thresh_1 = temp_img > 0.3
+    #     thresh_2 = temp_img <= 0.3
+    #     temp_img[thresh_1] = 1
+    #     temp_img[thresh_2] = 0
+        
+    #     temp_img = np.dstack(temp_img).T
+
+    #     prediction = predict_digit(temp_img, model)
+    #     prediction_list.append(str(prediction))
+
+    return prediction_list
+
+
+
+def _grab_image(path=None, stream=None, url=None):
+    # if the path is not None, then load the image from disk
+    if path is not None:
+        image = imread(path, cv2.IMREAD_GRAYSCALE)
+        # image = process_img(image)
+        
+    # otherwise, the image does not reside on disk
+    else:	
+        # if the URL is not None, then download the image
+        if url is not None:
+            resp = urllib.urlopen(url)
+            data = resp.read()
+        # if the stream is not None, then the image has been uploaded
+        elif stream is not None:
+            data = stream.read()
+        # convert the image to a NumPy array and then read it into
+        # OpenCV format
+        image = np.asarray(bytearray(data), dtype="uint8")
+        image = cv2.imdecode(image, cv2.IMREAD_GRAYSCALE)
+        # image = process_img(image)
+    # return the image
+    return image
 
 
 
