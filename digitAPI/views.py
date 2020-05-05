8c7388136795db3a64f07acc8af19ec28effff35
josenarvaezp <jose.narvaez.p@gmail.com>
2020-05-05 15:41:20 +0100

    make better

--- digitAPI/views.py
+++ digitAPI/views.py
@@ -343,7 +343,7 @@ def detect(request):
                 y1 = int(box[1])
                 x2 = int(box[2])
                 y2 = int(box[3])
-                data.update({"'"+"x1"+str(prediction_count)+"'": x1, "'"+"y1"+str(prediction_count)+"'": y1, "'"+"x2"+str(prediction_count)+"'": x2, "'"+"y2"+str(prediction_count)+"'": y2})
+                # data.update({"'"+"x1"+str(prediction_count)+"'": x1, "'"+"y1"+str(prediction_count)+"'": y1, "'"+"x2"+str(prediction_count)+"'": x2, "'"+"y2"+str(prediction_count)+"'": y2})
 
                 temp_img = image[y1:y2, x1:x2]
                 # data.update({"success": True ,"size2": temp_img.shape})
@@ -422,45 +422,45 @@ def extract_digits(img):
 def predict_digits(img, model):
 
     img = pre_process(img)
-    # coordinates = extract_digits(img)
+    coordinates = extract_digits(img)
     
-    temp_img = cv2.resize(img,(64,64))
+    # temp_img = cv2.resize(img,(64,64))
 
-    temp_img = np.dstack(temp_img).T
+    # temp_img = np.dstack(temp_img).T
 
-    prediction = predict_digit(temp_img, model)
+    # prediction = predict_digit(temp_img, model)
     prediction_list = []
-    prediction_list.append(str(prediction))
+    # prediction_list.append(str(prediction))
     
-    # for coordinate in coordinates:
-    #     l_x = coordinate[0]
-    #     r_x = coordinate[1]
+    for coordinate in coordinates:
+        l_x = coordinate[0]
+        r_x = coordinate[1]
 
-    #     if (r_x - l_x < 10):
-    #         continue
+        if (r_x - l_x < 10):
+            continue
 
-    #     temp_img = img[:,l_x:r_x]
-    #     y,x = temp_img.shape
+        temp_img = img[:,l_x:r_x]
+        y,x = temp_img.shape
 
-    #     # add half and half
-    #     to_add = int((y - x)/2)
-    #     diff = y - (x+ (to_add *2))
-    #     to_add_left = to_add
-    #     to_add_right = to_add + diff
+        # add half and half
+        to_add = int((y - x)/2)
+        diff = y - (x+ (to_add *2))
+        to_add_left = to_add
+        to_add_right = to_add + diff
         
-    #     temp_img = np.append(temp_img, np.zeros((y,to_add_left )), axis=1)
-    #     temp_img = np.append(np.zeros((y,to_add_right )),temp_img, axis=1)
+        temp_img = np.append(temp_img, np.zeros((y,to_add_left )), axis=1)
+        temp_img = np.append(np.zeros((y,to_add_right )),temp_img, axis=1)
 
-    #     temp_img = cv2.resize(temp_img,(64,64))
-    #     thresh_1 = temp_img > 0.3
-    #     thresh_2 = temp_img <= 0.3
-    #     temp_img[thresh_1] = 1
-    #     temp_img[thresh_2] = 0
+        temp_img = cv2.resize(temp_img,(64,64))
+        thresh_1 = temp_img > 0.3
+        thresh_2 = temp_img <= 0.3
+        temp_img[thresh_1] = 1
+        temp_img[thresh_2] = 0
         
-    #     temp_img = np.dstack(temp_img).T
+        temp_img = np.dstack(temp_img).T
 
-    #     prediction = predict_digit(temp_img, model)
-    #     prediction_list.append(str(prediction))
+        prediction = predict_digit(temp_img, model)
+        prediction_list.append(str(prediction))
 
     return prediction_list
 
